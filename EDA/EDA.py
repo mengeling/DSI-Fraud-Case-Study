19f376bb4c57969ec12bc8f86b841f8903fa6db7
Michael Engeling <michael.engeling@gmail.com>
2018-01-03 17:51:54 -0600

    Tweaked get data outputs

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -7,10 +7,6 @@ from model.pipeline import get_numeric_data
 
 
 def plot_histograms(df):
-    cols = [
-        "user_age", "body_length", "channels", "delivery_method", "fb_published", "has_analytics",
-        "num_payouts", "sale_duration2"
-    ]
     for col in cols:
         plt.hist(df[col], bins=50)
         plt.title(col)
@@ -26,6 +22,11 @@ def plot_scatter_matrix(df):
 
 if __name__ == '__main__':
     df = pd.read_json('../data/data.json')
+    cols = [
+        "user_age", "body_length", "channels", "delivery_method", "fb_published", "has_analytics",
+        "num_payouts", "sale_duration2"
+    ]
+    df = df[cols]
     df = get_numeric_data(df)
     plot_histograms(df)
     plot_scatter_matrix(df)
