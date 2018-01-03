e642b51a55ce7ff1e7398d6328c359dadcc144f7
Michael Engeling <michael.engeling@gmail.com>
2018-01-03 17:20:02 -0600

    Cleaned up pipeline

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -1,10 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 import sys
 sys.path.append('..')
 from model.pipeline import get_numeric_data
 
 
+def plot_histograms(df):
+    cols = [
+        "user_age", "body_length", "channels", "delivery_method", "fb_published", "has_analytics",
+        "num_payouts", "sale_duration2"
+    ]
+    for col in cols:
+        plt.hist(df[col], bins=50)
+        plt.title(col)
+        plt.xlabel(col)
+        plt.ylabel("Frequency")
+        plt.savefig("images/hist_{}.png".format(col))
 
 
 if __name__ == '__main__':
-    df = clean_numeric_data('../data/data.json')
-    df = clean_data(df)
+    df = pd.read_json('../data/data.json')
+    df = get_numeric_data(df)
+    plot_histograms(df)
--- model/pipeline.py
+++ model/pipeline.py
@@ -2,22 +2,29 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 
-def get_text_data(txt):
+def get_labels(series):
+    return (series != "premium").astype(int)
+
+
+def strip_html(txt):
     return BeautifulSoup(txt).text
 
 
+def get_text_data(df):
+    return df["description"].apply(strip_html).values
+
+
 def get_numeric_data(df):
-    df["fraud"] = (df["acct_type"] != "premium").astype(int)
     df["total_payout"] = df["ticket_types"].apply(unpack)
     df["listed"] = df["listed"].map({'y': 1, 'n': 0})
     cols = [
         "description", "country", "email_domain", "ticket_types", "venue_address", "venue_country",
         "venue_latitude", "has_header", "venue_longitude", "venue_name", "venue_state", "name", "object_id",
-        "org_desc", "org_facebook", "org_twitter", "org_name", "payee_name", "previous_payouts", "acct_type"
+        "org_desc", "org_facebook", "org_twitter", "org_name", "payee_name", "previous_payouts"
     ]
     df.drop(cols, axis=1, inplace=True)
     df.dropna(inplace=True)
-    return pd.get_dummies(df)
+    return pd.get_dummies(df).values
 
 
 def unpack(ticket_types_lst):
@@ -26,10 +33,10 @@ def unpack(ticket_types_lst):
 
 def main():
     df = pd.read_json('../data/data.json')
-    X_text = df["description"].apply(get_text_data).values
-    df = get_numeric_data(df)
-    y = df.pop('fraud').values
-    return X_text, df.values, y
+    y = get_labels(df.pop("acct_type"))
+    X_text = get_text_data(df)
+    X_num = get_numeric_data(df)
+    return X_text, X_num, y
 
 
 if __name__ == '__main__':
