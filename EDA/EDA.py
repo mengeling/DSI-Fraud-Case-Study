ddc841039b619800613949aebf96b628a39ea94e
Michael Engeling <michael.engeling@gmail.com>
2018-01-03 16:28:42 -0600

    Added pipeline file

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -1,34 +1,10 @@
-import pandas as pd
-from bs4 import BeautifulSoup
+import sys
+sys.path.append('..')
+from model.pipeline import clean_numeric_data
 
-def get_data(filename):
-    '''
-
-    parameters: filename
-    return: Pandas DataFrame
-
-    '''
-    return pd.read_json(filename)
-
-def extract_text(txt):
-    return BeautifulSoup(txt).text
-
-def clean_data(df):
-    df["fraud"] = (df["acct_type"] != "premium").astype(int)
-    cols = ["country", "email_domain", "ticket_types", "venue_address", "venue_country", "venue_latitude",
-             "has_header", "venue_longitude", "venue_name", "venue_state", "name", "object_id", "org_desc",
-             "org_facebook", "org_twitter", "org_name", "payee_name", "previous_payouts", "acct_type"]
-    df.drop(cols, axis=1, inplace=True)
-    df["listed"] = df["listed"].map({'y': 1, 'n': 0})
-    df.dropna(inplace=True)
-
-    df['description'] = df['description'].apply(extract_text)
-
-    return pd.get_dummies(df)
 
 
 
 if __name__ == '__main__':
-    df = get_data('../data/data.json')
-    numerical_df = clean_numerical_data(df)
-    text_df = clean_text_data(df)
+    df = clean_numeric_data('../data/data.json')
+    df = clean_data(df)
---
+++ model/pipeline.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+def read_file(filename):
+    '''
+    parameters: filename
+    return: Pandas DataFrame
+    '''
+    return pd.read_json(filename)
+
+
+def get_text_data(txt):
+    return BeautifulSoup(txt).text
+
+
+def get_numeric_data(df):
+    df["fraud"] = (df["acct_type"] != "premium").astype(int)
+    df["total_payout"] = df["ticket_types"].apply(unpack)
+    df["listed"] = df["listed"].map({'y': 1, 'n': 0})
+    cols = [
+        "description", "country", "email_domain", "ticket_types", "venue_address", "venue_country",
+        "venue_latitude", "has_header", "venue_longitude", "venue_name", "venue_state", "name", "object_id",
+        "org_desc", "org_facebook", "org_twitter", "org_name", "payee_name", "previous_payouts", "acct_type"
+    ]
+    df.drop(cols, axis=1, inplace=True)
+    df.dropna(inplace=True)
+    return pd.get_dummies(df)
+
+
+def unpack(ticket_types_lst):
+    return sum(d['quantity_sold'] * d['cost'] for d in ticket_types_lst)
+
+
+def main():
+    df = read_file('../data/data.json')
+    X_text = df["description"].apply(get_text_data).values
+    df = get_numeric_data(df)
+    y = df.pop('fraud').values
+    return X_text, df.values, y
+
+
+if __name__ == '__main__':
+    main()
