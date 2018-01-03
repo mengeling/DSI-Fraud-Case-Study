dca7eceb2a0be3520bae4a2363b84cbb2cad765d
Kim <kimdsorensen@gmail.com>
2018-01-03 15:09:06 -0600

    updated clean_data

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from bs4 import BeautifulSoup
 
 def get_data(filename):
     '''
@@ -10,19 +10,21 @@ def get_data(filename):
     '''
     return pd.read_json(filename)
 
+def extract_text(txt):
+    return BeautifulSoup(txt).text
 
-def clean_numerical_data(df):
+def clean_data(df):
     df["fraud"] = (df["acct_type"] != "premium").astype(int)
-    cols = ["country", "description", "email_domain", "ticket_types", "venue_address", "venue_country", "venue_latitude",
+    cols = ["country", "email_domain", "ticket_types", "venue_address", "venue_country", "venue_latitude",
              "has_header", "venue_longitude", "venue_name", "venue_state", "name", "object_id", "org_desc",
              "org_facebook", "org_twitter", "org_name", "payee_name", "previous_payouts", "acct_type"]
     df.drop(cols, axis=1, inplace=True)
     df["listed"] = df["listed"].map({'y': 1, 'n': 0})
     df.dropna(inplace=True)
+
+    df['description'] = df['description'].apply(extract_text)
+
     return pd.get_dummies(df)
-
-
-def clean_text_data(df):
 
 
 
