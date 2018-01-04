922eb529898b6d4a63a5e137f7fea2157651850d
Kim <kimdsorensen@gmail.com>
2018-01-04 11:38:51 -0600

    updated multiple files

--- model/model.py
+++ model/model.py
@@ -1,16 +1,18 @@
 import random
 import pickle
 import numpy as np
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
 from pipeline import main
+
 
 class MyModel():
     def __init__(self):
         self.vect = TfidfVectorizer()
         self.clas = MultinomialNB()
-        self.dt = DecisionTreeClassifier()
+        self.lr = LogisticRegression()
 
     def fit(self, X_text, X_num, y):
         #self.vect = TfidfVectorizer()
@@ -19,16 +21,20 @@ class MyModel():
         #clas = MultinomialNB()
         self.clas.fit(X_text, y)
 
-        self.dt.fit(X_num,y)
+        self.lr.fit(X_num,y)
 
-    def predict(self, X, y):
+    def predict(self, X_text, X_num):
 
-        pred_num = self.dt.predict_proba(X)
-        X_t = self._vectorizer.transform(X)
-        pred_txt = self.clas.predict_proba(X_t)
-        
-        return (np.array(pred_txt) + np.array(pred_num))/2
-
+        #X_text = df.pop('description')
+        #X_num = pd.get_dummies(df).values
+        #print(X_num)
+        pred_num = self.lr.predict_proba(X_num)[:, 1]
+        X_t = self.vect.transform(X_text)
+        pred_txt = self.clas.predict_proba(X_t)[:, 1]
+        #return np.mean(pred_num, pred_txt)
+        return (pred_txt[0] + pred_num[0])/2
+        #return (np.array(pred_txt) + np.array(pred_num))/2
+        #print(pred_num)
 
 #def get_data(datafile):
 #    return X, y
--- model/predict.py
+++ model/predict.py
@@ -1,12 +1,29 @@
 import pandas as pd
-import model
-import pipeline
+from model import MyModel
+from pipeline import clean_data
+import pickle
 
 df = pd.read_json('../data/example.json')
+df1 = df.head(1)
 
-X_text, X_num, y = pipeline.main()
+df1 = df1[["sale_duration2", "user_age", "body_length", "description","ticket_types"]]
+df = clean_data(df1)
+#y = df.pop("fraud")
+X_text = df.pop("description")
+X_num = df.values
+#print(df1)
 
-model = model.MyModel()
-model.fit(X_text,X_num,y)
+#data = clean_data(df1)
+#print(data)
+#y = data.pop('fraud')
+#, X_num, y = pipeline.main()
+#model = model.MyModel()
+#model.fit(X_text,X_num,y)
 
-print(df)
+
+with open('model.pkl', 'rb') as f:
+    m = pickle.load(f)
+
+    pred = m.predict(X_text, X_num)
+
+print(pred)
