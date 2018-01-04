87d034abee0f52012ba9735c5b3199a529a5bd6c
Michael Engeling <michael.engeling@gmail.com>
2018-01-04 12:41:28 -0600

    Hooked up predict function to API

--- model/model.py
+++ model/model.py
@@ -1,7 +1,4 @@
-import random
 import pickle
-import numpy as np
-import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
@@ -11,37 +8,26 @@ from pipeline import main
 class MyModel():
     def __init__(self):
         self.vect = TfidfVectorizer()
-        self.clas = MultinomialNB()
+        self.nb = MultinomialNB()
         self.lr = LogisticRegression()
 
     def fit(self, X_text, X_num, y):
-        #self.vect = TfidfVectorizer()
         X_text = self.vect.fit_transform(X_text)
-
-        #clas = MultinomialNB()
-        self.clas.fit(X_text, y)
-
+        self.nb.fit(X_text, y)
         self.lr.fit(X_num,y)
 
-    def predict(self, X_text, X_num):
-
-        #X_text = df.pop('description')
-        #X_num = pd.get_dummies(df).values
-        #print(X_num)
+    def predict_proba(self, X_text, X_num):
         pred_num = self.lr.predict_proba(X_num)[:, 1]
         X_t = self.vect.transform(X_text)
-        pred_txt = self.clas.predict_proba(X_t)[:, 1]
-        #return np.mean(pred_num, pred_txt)
+        pred_txt = self.nb.predict_proba(X_t)[:, 1]
         return (pred_txt[0] + pred_num[0])/2
-        #return (np.array(pred_txt) + np.array(pred_num))/2
-        #print(pred_num)
 
-#def get_data(datafile):
-#    return X, y
+    def predict(self, X_text, X_num):
+        proba = self.predict_proba(X_text, X_num)
+        return int(proba > 0.5)
 
 
 if __name__ == '__main__':
-    #X, y = get_data('data/data.json')
     X_text, X_num, y = main()
     model = MyModel()
     model.fit(X_text, X_num, y)
--- model/pipeline.py
+++ model/pipeline.py
@@ -11,22 +11,15 @@ def strip_html(txt):
     return BeautifulSoup(txt, "lxml").text
 
 
-def unpack(ticket_types_lst):
-    print(ticket_types_lst)
-    return sum(d['quantity_sold'] * d['cost'] for d in ticket_types_lst)
+def unpack(ticket_types):
+    if type(ticket_types) is dict:
+        return ticket_types['quantity_sold'] * ticket_types['cost']
+    return sum(d['quantity_sold'] * d['cost'] for d in ticket_types)
 
 
 def clean_data(df):
     df["total_payout"] = df["ticket_types"].apply(unpack)
-    # df["listed"] = df["listed"].map({'y': 1, 'n': 0})
     df["description"] = df["description"].apply(strip_html)
-    #cols = ["sale_duration2", "user_age", "payout_type", "total_payout", "body_length", "description", "fraud"]
-    #     "country", "email_domain", "ticket_types", "venue_address", "venue_country",
-    #     "venue_latitude", "has_header", "venue_longitude", "venue_name", "venue_state", "name", "object_id",
-    #     "org_desc", "org_facebook", "org_twitter", "org_name", "payee_name", "previous_payouts"
-    # ]
-    # df.drop(cols, axis=1, inplace=True)
-    #df = df[cols]
     df.drop("ticket_types", axis=1, inplace=True)
     df.dropna(inplace=True)
     return df
@@ -39,7 +32,6 @@ def main():
     y = df.pop("fraud")
     X_text = df.pop("description")
     X_num = df.values
-    #print(y)
     return X_text, X_num, y
 
 
