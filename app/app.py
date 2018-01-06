8ea0e407109f6e22125766ca43d4499edbf76a2b
Michael Engeling <michael.engeling@gmail.com>
2018-01-06 15:52:12 -0600

    Sorted fraud probabilities in descending order

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from datetime import datetime
 from flask import Flask, render_template
 
@@ -10,31 +11,36 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    high_risk, medium_risk, low_risk = retrieve_data_from_db()
+    df_high, df_medium, df_low = retrieve_data_from_db()
+    print(df_medium)
+    high_risk = df_high.sort_values(by='probability', ascending=False).values
+    medium_risk = df_medium.sort_values(by='probability', ascending=False).values
+    low_risk = df_low.sort_values(by='probability', ascending=False).values
+    print(high_risk)
     return render_template('index.html', high_risk=high_risk, medium_risk=medium_risk, low_risk=low_risk)
 
 
 def retrieve_data_from_db():
-    high_risk = []
-    medium_risk = []
-    low_risk = []
+    df_high = pd.DataFrame()
+    df_medium = pd.DataFrame()
+    df_low = pd.DataFrame()
     for i, d in enumerate(collection.find()):
         if i < 50:
             date = datetime.fromtimestamp(d["approx_payout_date"]).strftime('%Y-%m-%d')
             prediction = "Fraud" if d["prediction"] else "Not Fraud"
             if d["probability"] >= 0.8:
-                high_risk.append([
-                    d["org_name"], d["name"], date, round(d["probability"], 2), prediction
-                ])
+                df_high = df_high.append({"org": d["org_name"], "event": d["name"], "date": date,
+                                          "probability": round(d["probability"], 2), "prediction": prediction},
+                                         ignore_index=True)
             elif (d["probability"] > 0.5) and (d["probability"] < 0.8):
-                medium_risk.append([
-                    d["org_name"], d["name"], date, round(d["probability"], 2), prediction
-                ])
+                df_medium = df_medium.append({"org": d["org_name"], "event": d["name"], "date": date,
+                                              "probability": round(d["probability"], 2), "prediction": prediction},
+                                             ignore_index=True)
             else:
-                low_risk.append([
-                    d["org_name"], d["name"], date, round(d["probability"], 2), prediction
-                ])
-    return high_risk, medium_risk, low_risk
+                df_low = df_low.append({"org": d["org_name"], "event": d["name"], "date": date,
+                                        "probability": round(d["probability"], 2), "prediction": prediction},
+                                       ignore_index=True)
+    return df_high, df_medium, df_low
 
 
 if __name__ == '__main__':
